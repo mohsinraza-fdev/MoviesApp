d77999fb4daa5db7eef77160eb7bdd51469a1ad8
Mohsin Raza <m.mohsinraza@aol.com>
2023-12-10 20:17:53 +0500

    fixed video play in landscape

--- lib/presentation/dashboard/modules/watch/modules/watch_trailer/view/watch_trailer_view.dart
+++ lib/presentation/dashboard/modules/watch/modules/watch_trailer/view/watch_trailer_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ten_twenty_test/app/core/core_views/core_view.dart';
 import 'package:ten_twenty_test/presentation/dashboard/modules/watch/modules/watch_trailer/view_model/watch_trailer_view_model.dart';
+import 'package:ten_twenty_test/shared/enums/app_device_orientation_enum.dart';
+import 'package:ten_twenty_test/shared/helpers/app_device.dart';
 import 'package:ten_twenty_test/shared/helpers/utils.dart';
 import 'package:ten_twenty_test/widgets/others/app_loading_indicator.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
@@ -23,7 +25,10 @@ class WatchTrailerView extends CoreView<WatchTrailerViewModel> {
               child: YoutubePlayer(
                 controller: viewModel.controller!,
                 width: screenWidth(context),
-                aspectRatio: screenHeight(context) / screenWidth(context),
+                aspectRatio: AppDevice.of(context).orientation ==
+                        AppDeviceOrientation.landscape
+                    ? screenWidth(context) / screenHeight(context)
+                    : screenHeight(context) / screenWidth(context),
                 bottomActions: [
                   CurrentPosition(),
                   ProgressBar(isExpanded: true),
